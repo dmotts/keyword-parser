8d0d1e0c2cf57022812ba44c37c4475b1125a088
Daley Mottley <daley.mottley@hotmail.com>
2024-05-22 05:53:41 +0000

    Add the chrome driver binary location

--- modules/answerthepublic.py
+++ modules/answerthepublic.py
@@ -41,6 +41,8 @@ class Answer(Parser):
         chrome_options.add_argument('--no-sandbox')
         
         chrome_options.add_argument('--disable-dev-shm-usage')
+
+        chrome_options.binary_location = "/usr/bin/google-chrome" 
         
         chrome_options.add_argument('--remote-debugging-port=9222') 
 
