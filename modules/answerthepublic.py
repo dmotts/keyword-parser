62377f021412e75bd7d9026866b063a2c04830ae
Daley Mottley <daley.mottley@hotmail.com>
2024-05-22 14:27:37 +0000

    Change click search button function

--- modules/answerthepublic.py
+++ modules/answerthepublic.py
@@ -154,7 +154,7 @@ class Answer(Parser):
 
     def __click_search_button(self):
         # Locate the search button element by its class name ("search__submit").
-        element = self.driver._wait_for_element_clickable(By.CLASS_NAME, "search__submit")
+        element = self._wait_for_element_clickable(By.CLASS_NAME, "search__submit")
         
         # Simulate a click on the found search button element.
         element.click()
