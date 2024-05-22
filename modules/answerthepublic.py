9702438017eb84f2cfd591a8097cee9f9d6ba98f
Daley Mottley <daley.mottley@hotmail.com>
2024-05-22 06:02:13 +0000

    Add --disable-gpu chrome option

--- modules/answerthepublic.py
+++ modules/answerthepublic.py
@@ -39,6 +39,8 @@ class Answer(Parser):
 
     
         chrome_options.add_argument('--no-sandbox')
+
+        chrome_options.add_argument('--disable-gpu')
         
         chrome_options.add_argument('--disable-dev-shm-usage')
 
